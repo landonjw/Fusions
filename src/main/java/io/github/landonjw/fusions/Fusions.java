e4e76c0ec5ef107e5c2524d111fa2c183c3134ea
Hiroku <paul_l_preston@hotmail.com>
2021-10-31 22:25:19 +1000

    Unused import oops

--- src/main/java/io/github/landonjw/fusions/Fusions.java
+++ src/main/java/io/github/landonjw/fusions/Fusions.java
@@ -32,7 +32,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 /* -----------------------------------------------------------------------------------
  *                                Fusions by landonjw
